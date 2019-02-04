b34c6bebdc608028f7c0c786e1e3e47296330015
unknown <Paul Richards>
2019-02-04 17:52:36 -0600

    Added tetris example

--- buttonClass.py
+++ buttonClass.py
@@ -2,7 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 class ButtonsClass: 
-   def __init__(self):
+   def __init__(self, timeout = 0.1):
+      self.timeout = timeout
       GPIO.setmode(GPIO.BCM)
       self.pinList = {"TL":23, "TR":18, "Left":13, "Up":5, "Right":19, "Down":6, 
                       "Press":3, "Start":21, "Select":4, "Y":20, "B":12, "A":26, 
@@ -10,12 +11,16 @@ class ButtonsClass:
       # Sets the pin as input and sets Pull-up mode for the pin.
       for pin,number in self.pinList.items():
          GPIO.setup (number,GPIO.IN, GPIO.PUD_UP)
-
+      self.lastPress = time.time()
+      
    def checkKey (self,pin):
       pressed = False
-      pinNumber = self.pinList [pin]
-      if GPIO.input(pinNumber) == 0:
-         pressed = True
+      if (time.time() - self.lastPress) > self.timeout: 
+         pinNumber = self.pinList [pin]
+         if GPIO.input(pinNumber) == 0:
+            pressed = True
+            self.lastPress = time.time()
+         
       return pressed 
       
    def checkAll(self):         
@@ -27,7 +32,6 @@ class ButtonsClass:
 if __name__ == "__main__":
    buttons = ButtonsClass()
    while True:
-       time.sleep(0.05)
        buttons.checkAll()
        if buttons.checkKey  ("TL"): 
           print ("TL was pressed yo" )
